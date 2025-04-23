f95912443b199a57acafe7b7fabfc8c8c17b86ea
KDL <kdlefe@gmail.com>
2025-04-23 00:02:17 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -33,12 +33,9 @@
       </section>
 <section class="section-container" id="pilots">
   <div class="section-header-group">
-<section class="pilot-roster-section">
-  <div class="section-header clipped-medium-backward-pilot">
-    <img src="/icons/pilot-icon.svg" alt="Pilot Icon" class="pilot-icon" />
-    <h1 class="pilot-title">Pilot Roster</h1>
-    <div class="rhombus-back"></div>
-  </div>
+    <div class="section-header clipped-medium-backward-pilot">
+      <img src="/icons/pilot-icon.svg" />
+      <h1>Pilot Roster</h1>
   <div class="section-content-container">
     <div class="pilot-list-container">
       <Pilot v-for="item in pilots" :key="item.slug" :pilot="item" />
